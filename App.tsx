0e500d65d1942bb2107dd82b6e7f2302ccc79ba7
Talha-farooq <talhaafarooq123@gmail.com>
2022-03-05 11:52:18 +0500

    Calculator Part Done

---
+++ App.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+const App = () => {
+  const [num1, setnum1] = React.useState('');
+  const [num2, setnum2] = React.useState('');
+  const [operator, setOperator] = React.useState('');
+  const [result, setResult] = React.useState('');
+
+  const Cal = () => {
+    if (
+      operator === '+' ||
+      operator === '-' ||
+      operator === '-' ||
+      operator === '*' ||
+      operator === '/'
+    ) {
+      if (operator === '+') {
+        let temp: any = parseInt(num1) + parseInt(num2);
+        setResult(temp);
+        // console.log(temp);
+      }
+      if (operator === '-') {
+        let temp: any = parseInt(num1) - parseInt(num2);
+        setResult(temp);
+        //  console.log(temp);
+      }
+      if (operator === '*') {
+        let temp: any = parseInt(num1) * parseInt(num2);
+        setResult(temp);
+        //  console.log(temp);
+      }
+      if (operator === '/') {
+        let temp: any = parseInt(num1) / parseInt(num2);
+        setResult(temp);
+        //  console.log(temp);
+      }
+    } else {
+      Alert.alert('Invalid operator');
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.containerWraper}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={num => setnum1(num)}
+            keyboardType="number-pad"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={num => setOperator(num)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputStyle}
+            onChangeText={num => setnum2(num)}
+            keyboardType="number-pad"
+          />
+        </View>
+        <View style={{marginTop: 15}}>
+          <Text>=</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.inputStyle}>
+            <Text>{result === '' ? 0 : result}</Text>
+          </TextInput>
+        </View>
+      </View>
+
+      <TouchableOpacity onPress={() => Cal()} style={styles.btnResult}>
+        <Text style={styles.btnText}>Show Result</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerWraper: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#E5E5E5',
+    justifyContent: 'center',
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  inputContainer: {
+    borderWidth: 2,
+    marginBottom: 10,
+    borderRadius: 8,
+    borderColor: '#A52A2A',
+    fontSize: 16,
+  },
+
+  inputStyle: {
+    fontSize: 14,
+    paddingLeft: 10,
+  },
+  btnResult: {
+    backgroundColor: '#0000FF',
+    borderRadius: 10,
+    marginTop: 50,
+    paddingVertical: 10,
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 20,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+});
+export default App;
